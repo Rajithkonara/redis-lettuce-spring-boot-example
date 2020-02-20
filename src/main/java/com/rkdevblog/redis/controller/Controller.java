b0a583a2e234f147a24b5e07a63cd10506a31e4c
rajithkonara <rajith.konara@tokoin.io>
2020-02-20 21:16:52 +0530

    adding the controller

--- src/main/java/com/rkdevblog/redis/controller/Controller.java
+++ src/main/java/com/rkdevblog/redis/controller/Controller.java
@@ -1,4 +1,49 @@
 package com.rkdevblog.redis.controller;
 
+import com.rkdevblog.redis.dto.OtpRequest;
+import com.rkdevblog.redis.dto.OtpValidateRequest;
+import com.rkdevblog.redis.repository.CacheRepository;
+import com.rkdevblog.redis.util.OtpGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/otp")
 public class Controller {
+
+    private final CacheRepository cacheRepository;
+    private final OtpGenerator otpGenerator;
+
+    @Autowired
+    public Controller(CacheRepository cacheRepository, OtpGenerator otpGenerator) {
+        this.cacheRepository = cacheRepository;
+        this.otpGenerator = otpGenerator;
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> addToCache(@RequestBody OtpRequest key) {
+
+        int value = otpGenerator.generateOtp();
+        cacheRepository.put(key.getEmail(), value);
+
+        return ResponseEntity.ok("Otp Generated Successfully Otp : " + value);
+    }
+
+
+    @PostMapping("/verify")
+    public ResponseEntity<String> removeFromCache(@RequestBody OtpValidateRequest otpValidateRequest) {
+
+        Optional<String> s = cacheRepository.get(otpValidateRequest.getKey());
+
+        if (s.isPresent() && s.get().equals(otpValidateRequest.getOtp())) {
+            cacheRepository.remove(otpValidateRequest.getKey());
+            return ResponseEntity.ok("Key Removed from cache key:  " + otpValidateRequest.getKey());
+        }
+
+        return ResponseEntity.badRequest().body("Invalid Otp");
+    }
+
 }
